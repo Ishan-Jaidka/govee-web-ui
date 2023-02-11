5b3599e47e26111ae75d2aeee9d5df8b026ba093
Ishan Jaidka <ijaidka@gmail.com>
2023-02-11 03:15:03 -0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./components/router";
-import { UserAPIContext } from "./contexts/user";
 
 function App() {
   return (
-    <UserAPIContext value="">
-      <div className="App">
-        <RouterProvider router={router} />
-      </div>
-    </UserAPIContext>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
