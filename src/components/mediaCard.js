210d4180130d2db6fe441b80a66e60b84161ec25
Ishan Jaidka <ijaidka@gmail.com>
2023-02-13 09:47:27 -0800

    add card on/off buttons

--- src/components/mediaCard.js
+++ src/components/mediaCard.js
@@ -5,23 +5,50 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { CardActionArea, CardActions } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useGoveeKey } from "../contexts/GoveeKeyContext";
 
 export default function MediaCard({
-  width = 395,
+  width = 275,
   maxWidth = "90vw",
   margin = 1,
   data = null, // {deviceName, device, model, properties, supportCmds}
 }) {
+  const goveeKey = useGoveeKey();
   const navigate = useNavigate();
   const cardClicked = () => {
     navigate(`/devices/${data.deviceName}`, { state: data });
   };
 
+  function deviceOn(on) {
+    const config = {
+      headers: {
+        "Govee-API-Key": goveeKey,
+      },
+    };
+    const body = {
+      device: data.device,
+      model: data.model,
+      cmd: {
+        name: "turn",
+        value: on ? "on" : "off",
+      },
+    };
+    axios
+      .put("https://developer-api.govee.com/v1/devices/control", body, config)
+      .then((data) => {
+        // if (data.status === 200) setPower(on ? "on" : "off");
+      })
+      .catch((err) => {
+        alert("Something went wrong. Please try again later.");
+        console.log(err);
+      });
+  }
+
   return (
     <Card
       sx={{ width: width, margin: margin, maxWidth: maxWidth }}
       variant="outlined"
-      raised={true}
     >
       <CardActionArea onClick={cardClicked}>
         <CardContent>
@@ -38,23 +65,14 @@ export default function MediaCard({
         </CardContent>
       </CardActionArea>
 
-      {/* <CardActions>
-        {card_action_url && (
-          <Button size="small" color="primary" href={card_action_url}>
-            GitHub
-          </Button>
-        )}
-        {view_page_url && (
-          <Button size="small" color="primary" href={view_page_url}>
-            View Page
-          </Button>
-        )}
-        {data.awards && (
-          <Button size="small" color="primary" href={data.awards}>
-            View Awards
-          </Button>
-        )}
-      </CardActions> */}
+      <CardActions sx={{ justifyContent: "space-around" }}>
+        <Button size="small" color="primary" onClick={() => deviceOn(true)}>
+          On
+        </Button>
+        <Button size="small" color="primary" onClick={() => deviceOn(false)}>
+          Off
+        </Button>
+      </CardActions>
     </Card>
   );
 }
