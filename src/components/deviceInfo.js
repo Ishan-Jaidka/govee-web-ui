e45fab77afc28da496651f4c5614c115e2c41940
Ishan Jaidka <ijaidka@gmail.com>
2023-02-13 02:51:58 -0800

    device data + controls

---
+++ src/components/deviceInfo.js
@@ -0,0 +1,65 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useGoveeKey } from "../contexts/GoveeKeyContext";
+import "./deviceInfo.css";
+
+export default function DeviceInfo({
+  mac,
+  model,
+  online,
+  powerState,
+  colorTem,
+  brightness,
+}) {
+  const goveeKey = useGoveeKey();
+  const [power, setPower] = useState(powerState);
+
+  function deviceOn(on) {
+    const config = {
+      headers: {
+        "Govee-API-Key": goveeKey,
+      },
+    };
+    const body = {
+      device: mac,
+      model: model,
+      cmd: {
+        name: "turn",
+        value: on ? "on" : "off",
+      },
+    };
+    axios
+      .put("https://developer-api.govee.com/v1/devices/control", body, config)
+      .then((data) => {
+        if (data.status === 200) setPower(on ? "on" : "off");
+      })
+      .catch((err) => {
+        alert("Something went wrong. Please try again later.");
+        console.log(err);
+      });
+  }
+  return (
+    <div className="deviceinfo-outerdiv">
+      <div>
+        <table>
+          <tr>
+            <th>Online</th>
+            <th>Power</th>
+            {colorTem && <th>Color</th>}
+            {brightness && <th>Brightness</th>}
+          </tr>
+          <tr>
+            <td>{online.toString()}</td>
+            <td>{power}</td>
+            {colorTem && <td>{colorTem}</td>}
+            {brightness && <td>{brightness}</td>}
+          </tr>
+        </table>
+      </div>
+      <div>
+        <button onClick={() => deviceOn(true)}>Turn On</button>
+        <button onClick={() => deviceOn(false)}>Turn Off</button>
+      </div>
+    </div>
+  );
+}
--- src/views/devicePage.js
+++ src/views/devicePage.js
@@ -1,22 +1,72 @@
-import React from "react";
-import { useLocation } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import DeviceInfo from "../components/deviceInfo";
+import { useGoveeKey } from "../contexts/GoveeKeyContext";
 
 export default function DevicePage() {
   const { state } = useLocation();
+  const goveeKey = useGoveeKey();
+  const [status, setStatus] = useState("Loading");
+  const navigate = useNavigate();
+  const [deviceInfo, setDeviceInfo] = useState(null);
 
-  //   useEffect(() => {
-  //     const config = {
-  //         headers: {
-  //           "Govee-API-Key": searchParams.get("key"),
-  //         },
-  //       };
-  //     axios.get(`https://developer-api.govee.com/v1/devices/state?device=${state.device}&model=${state.model}`, )
-  //   }, [])
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Govee-API-Key": goveeKey,
+      },
+    };
+    axios
+      .get(
+        `https://developer-api.govee.com/v1/devices/state?device=${state.device}&model=${state.model}`,
+        config
+      )
+      .then((data) => {
+        // console.log(data.data.data.properties);
+        if (data.status === 200) {
+          const deviceProperties = data.data.data.properties;
+          const device = (
+            <DeviceInfo
+              mac={state.device}
+              model={state.model}
+              online={deviceProperties[0]?.online ?? false}
+              powerState={deviceProperties[1]?.powerState ?? null}
+              brightness={deviceProperties[2]?.brightness ?? null}
+              colorTem={deviceProperties[4]?.colorTem ?? null}
+            />
+          );
+          setDeviceInfo(device);
+          setStatus("Success");
+        } else setStatus("Error");
+      })
+      .catch((err) => {
+        console.log(err);
+        setStatus("Error");
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div>
-      <div>{state.deviceName}</div>
+      <h1>{state.deviceName}</h1>
       <div>Mac Address: {state.device}</div>
       <div>Model: {state.model}</div>
+      {status === "Success" && <div>{deviceInfo}</div>}
+      {status === "Loading" && (
+        <div>
+          <br />
+          <br />
+          Loading...
+        </div>
+      )}
+      {status === "Error" && (
+        <>
+          <br />
+          <br />
+          <h3>Something went wrong. Please try again.</h3>
+          <button onClick={() => navigate("/")}>Home</button>
+        </>
+      )}
     </div>
   );
 }
