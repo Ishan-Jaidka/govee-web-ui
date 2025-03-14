6e3aa1644d125cb117608b245e2918a2d68beef2
Ishan Jaidka <ijaidka@gmail.com>
2025-03-14 16:13:07 -0700

    brightness slider

--- src/components/deviceInfo.js
+++ src/components/deviceInfo.js
@@ -1,6 +1,6 @@
-import { Button } from "@mui/material";
+import { Box, Button, Slider } from "@mui/material";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { useGoveeKey } from "../contexts/GoveeKeyContext";
 import "./deviceInfo.css";
 
@@ -14,6 +14,8 @@ export default function DeviceInfo({
 }) {
   const goveeKey = useGoveeKey();
   const [power, setPower] = useState(powerState);
+  const [bright, setBrightness] = useState(brightness);
+  const debounceRef = useRef(null);
 
   function deviceOn(on) {
     const config = {
@@ -36,8 +38,35 @@ export default function DeviceInfo({
       })
       .catch((err) => {
         alert("Something went wrong. Please try again later.");
-        console.log(err);
+        console.error(err);
       });
+  }
+  function brightnessChanged(val) {
+    if (debounceRef.current) clearTimeout(debounceRef.current);
+    debounceRef.current = setTimeout(() => {
+      const config = {
+        headers: {
+          "Govee-API-Key": goveeKey,
+        },
+      };
+      const body = {
+        device: mac,
+        model: model,
+        cmd: {
+          name: "brightness",
+          value: val,
+        },
+      };
+      axios
+        .put("https://developer-api.govee.com/v1/devices/control", body, config)
+        .then((data) => {
+          if (data.status === 200) setBrightness(val);
+        })
+        .catch((err) => {
+          alert("Something went wrong. Please try again later.");
+          console.error(err);
+        });
+    }, 1000);
   }
   return (
     <div className="deviceinfo-outerdiv">
@@ -53,10 +82,20 @@ export default function DeviceInfo({
             <td>{online.toString()}</td>
             <td>{power}</td>
             {colorTem && <td>{colorTem}</td>}
-            {brightness && <td>{brightness}</td>}
+            {brightness && <td>{bright}</td>}
           </tr>
         </table>
       </div>
+      {brightness && (
+        <Box sx={{ width: 1 }}>
+          <Slider
+            defaultValue={bright}
+            step={5}
+            valueLabelDisplay="on"
+            onChange={(e, val) => brightnessChanged(val)}
+          />
+        </Box>
+      )}
       <div className="deviceinfo-controldiv">
         <Button
           size="medium"
